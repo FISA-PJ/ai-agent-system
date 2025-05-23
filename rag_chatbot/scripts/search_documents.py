015d2e1931f2e1a0f460290e8d8e3efd1ee86862
MINJAEKH <chamj0513@gmail.com>
2025-05-23 11:12:56 +0900

    Fix:버그 수정

--- rag_chatbot/scripts/search_documents.py
+++ rag_chatbot/scripts/search_documents.py
@@ -2,8 +2,6 @@ from torch import Tensor
 import torch.nn.functional as F
 from transformers import AutoTokenizer, AutoModel
 
-from elasticsearch import Elasticsearch
-
 #------ query Embedding ------
 def average_pool(last_hidden_states: Tensor, attention_mask: Tensor) -> Tensor:
     last_hidden = last_hidden_states.masked_fill(~attention_mask[..., None].bool(), 0.0)
@@ -26,16 +24,54 @@ def get_embedding(text, model_name):
     return embeddings.tolist()
 
 
+# # 벡터 유사도 기반의 문서 검색
+# def search_documents(query, es, index_name, model_name, apt_code) :
+#     query_embedding = get_embedding(query, model_name)[0]
+    
+#     body = {
+#             "query": {
+#                 "bool": {
+#                     "must": [
+#                         {
+#                             "knn": {
+#                                 "field": "embedding",
+#                                 "query_vector": query_embedding,
+#                                 "k": 3,
+#                                 "num_candidates": 5
+#                             }
+#                         },
+#                         {
+#                             "term": {
+#                                 "apt_code.keyword": apt_code
+#                             }
+#                         }
+#                     ]
+#                 }
+#             }
+#         }
+#     results = es.search(index=index_name, body=body)
+    
+#     #return [hit["_source"]['content'] for hit in results["hits"]["hits"]]
+#     return [
+#         {
+#             "id": hit["_id"],
+#             "content": hit["_source"]["content"],
+#             "source_pdf": hit["_source"].get("source_pdf", "알 수 없음")
+#         }
+#         for hit in results["hits"]["hits"]
+#     ]
+
+
 # 벡터 유사도 기반의 문서 검색
 def search_documents(query, es, index_name, model_name) :
     query_embedding = get_embedding(query, model_name)[0]
     results = es.search(index=index_name, body={
         "knn": {
-            "field": "embedding",
+            "field": "vector",
             "query_vector": query_embedding,
-            "k": 5,
+            "k": 3,
             "num_candidates": 10
         }
     })
     
-    return [hit["_source"]['content'] for hit in results["hits"]["hits"]]
+    return [hit["_source"]['text'] for hit in results["hits"]["hits"]]
