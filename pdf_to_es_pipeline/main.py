3a231bdf9a64687980c501624c11414f5c499258
MINJAEKH <chamj0513@gmail.com>
2025-05-24 12:13:10 +0900

    Refactor: es 삭제 쿼리 예외처리

--- pdf_to_es_pipeline/main.py
+++ pdf_to_es_pipeline/main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from langchain.schema import Document
 from elasticsearch import Elasticsearch
+from elasticsearch import NotFoundError, TransportError
 from langchain_elasticsearch import ElasticsearchStore
 
 from processor1_pdf2md import Parser2Markdown
@@ -9,8 +10,8 @@ from processor2_chunking import HeaderSplitter, SemanticSplitter
 from processor3_embedding import BgeM3Embedding
 
 load_dotenv()
-#os.chdir('../experiment_results_upstage/') # 이건 시험용 (이미 파싱된 HTML 문서 폴더)
-os.chdir('../data/')
+os.chdir('experiment_results_upstage/') # 이건 시험용 (이미 파싱된 HTML 문서 폴더)
+#os.chdir('../data/')
 
 # 변수 생성
 UPSTAGE_API_KEY = os.getenv("UPSTAGE_API_KEY") 
@@ -20,19 +21,19 @@ embeddings = BgeM3Embedding()
 header_splitter = HeaderSplitter()
 second_splitter = SemanticSplitter(embeddings)
 es =  Elasticsearch('http://localhost:9200')
-
+index_name = "test-0524-tmp"
 
 vectorstore = ElasticsearchStore(
-    index_name="test-0524",
+    index_name=index_name,
     embedding=embeddings,
     es_connection=es,
 )
 
     
-# # 이미 파싱된 파일 읽어오는 경우
-# def read_md_file(file_path):
-#     with open(file_path, 'r', encoding='utf-8') as f:
-#         return f.read()
+# 이미 파싱된 파일 읽어오는 경우
+def read_md_file(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        return f.read()
 
 
 for file_name in os.listdir('.'):
@@ -41,20 +42,35 @@ for file_name in os.listdir('.'):
     
     ## 1. 기존 문서 삭제 (Elasticsearch 쿼리)
     apt_code = file_name.split('_')[0] # file_name
-    delete_query = {
-        "query": {
-            "term": {
-                "metadata.apt_code": apt_code
+    try:
+        if es.indices.exists(index=index_name):
+            delete_query = {
+                "query": {
+                    "term": {
+                        "metadata.apt_code": apt_code
+                    }
+                }
             }
-        }
-    }
-    es.delete_by_query(index="test-0524", body=delete_query)
-    print(f"▶INFO: 🗑️ 기존 apt_code={apt_code} 문서가 Elasticsearch에서 삭제되었습니다.")
-    
-    
+            response = es.delete_by_query(index=index_name, body=delete_query)
+            deleted_count = response.get("deleted", 0)
+            print(f"▶ INFO: 🗑️ apt_code={apt_code} 관련 문서 {deleted_count}개 삭제 완료")
+        else:
+            print(f"▶ INFO: ⚠️ 인덱스 {index_name}가 존재하지 않아 삭제를 건너뜁니다.")
+
+    except NotFoundError as e:
+        print(f"▶ INFO: 🚫 삭제 실패: (문서 없음) {e}")
+
+    except TransportError as e:
+        print(f"▶ INFO: 🚨 Elasticsearch 연결 또는 요청 실패: {e.error}, 상태 코드: {e.status_code}")
+
+    except Exception as e:
+        print(f"❗알 수 없는 예외 발생: {e}")
+
+
     ## 2. PDF문 파싱 및 마크다운 형태로 변환  
     #html_contents = preprocessor.pdf_upstageparser(file_name)
-    html_contents = preprocessor.pdf_openparse(file_name)
+    #html_contents = preprocessor.pdf_openparse(file_name)
+    html_contents = read_md_file(file_name)
     markdown_texts = preprocessor.html2md_with_spans(html_contents) 
 
     doc = Document(
