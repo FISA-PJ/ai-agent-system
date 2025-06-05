ccd89521ee4cc5892894380272f2a29dd912eada
RMaeng2 <erika990809@naver.com>
2025-06-06 02:30:24 +0900

    Chore : 대출 기능 사용 변수 및 함수 추가

--- tools/__init__.py
+++ tools/__init__.py
@@ -3,8 +3,10 @@ from config.config import intent_llm
 from prompts.fallback_answering_prompt import FALLBACK_ANSWER
 from prompts.irrelevant_answering_prompt import CLASSIFY_IRR_MMS_TYPE_PROMPT, GREETING_PROMPT, IRRELEVANT_ANSWER
 from prompts.intent_classification_prompt import INTENT_PROMPT
-from prompts.housing_react_agent_prompt import react_housing_prompt 
+from prompts.housing_react_agent_prompt import react_housing_prompt
+from prompts.loan_prompt_template import prompt_user, prompt_notice
 from agents.housing_react_agent import housing_react_agent
+from db.db import get_db_connection
 
 __all__ = [
     'es_client',
@@ -18,4 +20,7 @@ __all__ = [
     'FALLBACK_ANSWER',
     'react_housing_prompt',
     'housing_react_agent',
+    'get_db_connection', 
+    'prompt_user',
+    'prompt_notice',
 ]
